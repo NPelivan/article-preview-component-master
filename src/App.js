5d63b8e7b14daf86e069fb58b1c05bc1a58dd868
Nikolina Pelivan <nikolina_pelivan@hotmail.com>
2020-07-08 14:48:10 +0200

    Bug in onClick event

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Drawers from "./components/images/drawers.jpg";
+import User from "./components/images/avatar-michelle.jpg";
+/*import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";*/
 
-function App() {
+function share(e) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      SHARE
+      <a href="http://www.facebook.com">facebook</a>
     </div>
   );
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <body className="container">
+        <img src={Drawers} alt="drawers" />
+
+        <h2>
+          Shift the overall look and feel by adding these wonderful touches to
+          furniture in your home
+        </h2>
+
+        <p>
+          Ever been in a room and felt like something was missing? Perhaps it
+          felt slightly bare and uninviting. I’ve got some simple tips to help
+          you make any room feel complete.
+        </p>
+
+        <img src={User} alt="User: Michelle" />
+
+        <span>Michelle Appleton</span>
+        <span>28 Jun 2020</span>
+
+        <button onClick={this.share}>Share</button>
+      </body>
+    );
+  }
 }
 
 export default App;
